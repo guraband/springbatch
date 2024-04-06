8f95eba4d9a4d0ec37ec0bf55b66069dfae5b298
NG <guraband@naver.com>
2024-04-07 00:49:54 +0900

    tasklet, chunk 테스트

--- src/main/kotlin/com/fastcampus/springbatch/config/JobConfiguration.kt
+++ src/main/kotlin/com/fastcampus/springbatch/config/JobConfiguration.kt
@@ -3,9 +3,13 @@ package com.fastcampus.springbatch.config
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.batch.core.Job
 import org.springframework.batch.core.Step
+import org.springframework.batch.core.StepContribution
 import org.springframework.batch.core.job.builder.JobBuilder
 import org.springframework.batch.core.repository.JobRepository
+import org.springframework.batch.core.scope.context.ChunkContext
 import org.springframework.batch.core.step.builder.StepBuilder
+import org.springframework.batch.core.step.tasklet.Tasklet
+import org.springframework.batch.item.ItemReader
 import org.springframework.batch.repeat.RepeatStatus
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -17,21 +21,55 @@ private val logger = KotlinLogging.logger {}
 class JobConfiguration {
     @Bean
     fun job(jobRepository: JobRepository, step: Step): Job {
-        return JobBuilder("job", jobRepository)
+        return JobBuilder("job-chunk", jobRepository)
             .start(step)
             .build()
     }
 
     @Bean
-    fun step(
-        jobRepository: JobRepository,
-        platformTransactionManager: PlatformTransactionManager
-    ): Step {
+    fun step(jobRepository: JobRepository, platformTransactionManager: PlatformTransactionManager): Step {
+        val tasklet: Tasklet = object : Tasklet {
+            private var count = 0
+
+            @Throws(Exception::class)
+            override fun execute(contribution: StepContribution, chunkContext: ChunkContext): RepeatStatus {
+                count++
+                return if (count == 15) {
+                    logger.info { "job finished" }
+                    RepeatStatus.FINISHED
+                } else {
+                    logger.info { "job continuable $count" }
+                    RepeatStatus.CONTINUABLE
+                }
+            }
+        }
+
+        val itemReader = object : ItemReader<Int?> {
+            private var count = 0
+
+            override fun read() : Int? {
+                count++
+
+                logger.info { "read $count"}
+
+                if (count == 15) {
+                    logger.info { "read finished" }
+                    return null
+                }
+
+                return count
+            }
+        }
+        /*
         return StepBuilder("step", jobRepository)
-            .tasklet({ _, _ ->
-                logger.info { "step" }
-                RepeatStatus.FINISHED
-            }, platformTransactionManager)
+            .tasklet(tasklet, platformTransactionManager)
+            .build()
+        */
+        return StepBuilder("step", jobRepository)
+            .chunk<Any, Any>(10, platformTransactionManager)
+            .reader(itemReader)
+//            .processor()
+            .writer { }
             .build()
     }
 }
