578a5f5162cb5179a9d61ef95f3a4e035c399b49
NG <guraband@naver.com>
2024-04-08 23:48:54 +0900

    JpaItemReader test

--- src/main/kotlin/com/fastcampus/springbatch/config/ItemReaderJobConfiguration.kt
+++ src/main/kotlin/com/fastcampus/springbatch/config/ItemReaderJobConfiguration.kt
@@ -2,6 +2,7 @@ package com.fastcampus.springbatch.config
 
 import com.fastcampus.springbatch.dto.User
 import io.github.oshai.kotlinlogging.KotlinLogging
+import jakarta.persistence.EntityManagerFactory
 import org.springframework.batch.core.Job
 import org.springframework.batch.core.Step
 import org.springframework.batch.core.job.builder.JobBuilder
@@ -9,6 +10,7 @@ import org.springframework.batch.core.launch.support.RunIdIncrementer
 import org.springframework.batch.core.repository.JobRepository
 import org.springframework.batch.core.step.builder.StepBuilder
 import org.springframework.batch.item.ItemReader
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder
 import org.springframework.batch.item.file.FlatFileItemReader
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder
 import org.springframework.context.annotation.Bean
@@ -35,11 +37,12 @@ class ItemReaderJobConfiguration {
     fun step(
         jobRepository: JobRepository,
         platformTransactionManager: PlatformTransactionManager,
-        flatFileItemReader: ItemReader<User>,
+//        flatFileItemReader: ItemReader<User>,
+        jpaPagingItemReader: ItemReader<User>,
     ): Step {
         return StepBuilder("itemReaderStep", jobRepository)
-            .chunk<User, User>(2, platformTransactionManager)
-            .reader(flatFileItemReader)
+            .chunk<User, User>(1000, platformTransactionManager)
+            .reader(jpaPagingItemReader)
             .writer(System.out::println)
             .allowStartIfComplete(true)
             .build()
@@ -55,5 +58,17 @@ class ItemReaderJobConfiguration {
                 .names("name", "age", "region", "phoneNo")
                 .targetType(User::class.java)
                 .build()
+    }
+
+    @Bean
+    fun jpaPagingItemReader(
+        entityManagerFactory: EntityManagerFactory
+    ) : ItemReader<User> {
+        return JpaPagingItemReaderBuilder<User>()
+            .name("jpaPagingItemReader")
+            .entityManagerFactory(entityManagerFactory)
+            .pageSize(50)
+            .queryString("select u from USER u order by u.id")
+            .build()
     }
 }
--- src/main/kotlin/com/fastcampus/springbatch/dto/User.kt
+++ src/main/kotlin/com/fastcampus/springbatch/dto/User.kt
@@ -1,8 +1,17 @@
 package com.fastcampus.springbatch.dto
 
+import jakarta.persistence.*
+
+@Entity(name = "USER")
+@Table(name = "USER")
 data class User(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null,
     var name: String? = null,
     var age: String? = null,
     var region: String? = null,
+
+    @Column(name="phone_no")
     var phoneNo: String? = null,
 )
